c51f8579840a725994a604d55c702efdb382325e
Ivanova1233 <elizaveta2008god@gmail.com>
2025-05-15 10:18:04 +0300

    Add files via upload

--- task5.py
+++ task5.py
@@ -1,58 +1,38 @@
- a = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-b = len(a)
-print(b)
-c = [2, -93, -2, 8, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-d = sum(c)
-r = d//19
-a[4] = -17
-print(a)
+# a = [10, 8, 9, 7, 6, 9, 10, 8, 9, 10]
+# b = sum(a)
+# c = len(a)
+# d = b//c
+# print(d)
+# v = min(a)
+# f = max(a)
+# print(v)
+# print(f)
 
-a = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
-print( a [ : 3 ])
-print( a [3: ])
+# a = ["яблоко", "банан", "опельсин", "виноград"]
+# a[2] = 'апельсин'
+# print(a)
 
-a = ["Письмо 1", "Письмо 2", "Письмо 3", "Письмо 4", "Письмо 5"]
-a.reverse()
-print(a)
+# speed = 4096 / 1024
+# time = 120 * 60
+# coast = 0.125
+# free = 500
+# file = speed * time
+# money = (file-free)*coast
+# print(file)
+# print(money)
 
-e = ["Письмо 1", "Письмо 2", "Письмо 3", "Письмо 4", "Письмо 5"]
-e[2]
-e[3]
-print(e[2])
-print(e[3])
+# money = 10000
+# add = 5000
+# money +=add
+# print(money)
 
-a = [10, 8, 9, 7, 6, 9, 10, 8, 9, 10]
-b = sum(a)
-c = len(a)
-d = b//c
-print(d)
-v = min(a)
-f = max(a)
-print(v)
-print(f)
-
-a = ["яблоко", "банан", "опельсин", "виноград"]
-a[2] = 'апельсин'
-print(a)
-
-speed = 4096 / 1024
-time = 120 * 60
-coast = 0.125
-free = 500
-file = speed * time
-money = (file-free)*coast
-print(file)
-print(money)
-
-money = 10000
-add = 5000
-money +=add
-print(money)
-
-users =['user1', 'user2', 'user3', 'user1', 'user4', 'user2']
-dict_ ={"Общее количество": 0, "Уникальные посещения": 0}
-a = len(users)
-c = set(users)
-print(c)
-dict_1 ={"Общее количество": a, "Уникальные посещения": len(c) }
-print(dict_1)
+# users =['user1', 'user2', 'user3', 'user1', 'user4', 'user2']
+# dict_ ={"Общее количество": 0, "Уникальные посещения": 0}
+# a = len(users)
+# c = set(users)
+# print(c)
+# dict_1 ={"Общее количество": a, "Уникальные посещения": len(c) }
+# print(dict_1)
+# group_124 = [ 'Дарья','София', 'Елизавета', 'Фатима', 'Эвелина', 'Элеонора','Алина','Маргарита','Полина','Юрий','Илья','Екатерина'
+# #print(group_124[ : 6 ])
+# #print(group_124[ 6 :  ])
